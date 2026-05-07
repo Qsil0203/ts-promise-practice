1013631cba400c95b2de1f1b36dabe4748e98436
Admin <Admin@DESKTOP-EFA0P2J>
2026-05-08 02:07:32 +0500

    first-commit

--- src/posts.ts
+++ src/posts.ts
@@ -7,11 +7,11 @@ export function renderPosts() {
   posts.forEach((post: Post) => {
     const li = document.createElement('li')
     li.innerHTML = `
-      <div style="border: 1px solid black; border-radius: 10px; padding: 10px;">
-        <h3>${post.title}</h3>
-        <p>${post.body}</p>
-        <span>Link: <a href="${post.link}">Ссылка на пост</a></span>
-      </div>
+      <li class="post-card">
+        <div class="post-title">${post.title}</div>
+        <div class="post-body">${post.body}</div>
+        <a class="post-link " href="${post.link}">Link: ${post.link}</a>
+    </li>
     `
     document.getElementById("list")?.appendChild(li)
   })
